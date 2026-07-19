84e265ebdf0774204b0730441354a58d0b97df29
TheRudraBro <rudramraj22@gmail.com>
2026-07-19 18:03:16 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,16 @@ function App() {
 
   return (
     <>
+    <div className= 'bg-slate-800'>
     <h2 className = 'bg-red-600'> Hello Fucker </h2>
-    <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
+    <div className="card bg-slate-900 shadow-sm">
+      <div className="card-body text-white">
+
+<h2>Blood Finder</h2>
+<p>Donor registry, search, request management and AI matching score.</p>
+
+      </div>
+</div>
 </div>
 
     </>
